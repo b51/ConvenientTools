824f885875bec6633e9ba37d6de2e6905fc5e9a7
b51 <b51live@gmail.com>
2017-11-30 11:16:30 +0800

    Add -y to apt install and move Caps-Ctrl swap to the end.

--- InstallEnvOnUbuntu/installEnv.sh
+++ InstallEnvOnUbuntu/installEnv.sh
@@ -40,6 +40,24 @@ python install.py
 cd $DIRROOT
 cp GitSettings/.gitconfig $HOME
 
+# Install ROS
+/bin/echo -e "\e[1;32mInstalling ROS Kinetic.\e[0m"
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+sudo apt-get update -y
+sudo apt-get install ros-kinetic-desktop -y
+sudo rosdep init
+rosdep update
+
+tee -a ~/.bashrc > /dev/null <<'EOF'
+source /opt/ros/kinetic/setup.bash
+ros_ip=`ifconfig wlan0` | grep "inet addr" | awk -F : "{print $2}" | awk "{print $1}"
+export ROS_IP=$ros_ip
+EOF
+
+source ~/.bashrc
+sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential python-catkin-tools -y
+
 # Caps Ctrl exchange
 /bin/echo -e "\e[1;32mMaking Caps as Ctrl.\e[0m"
 sudo tee -a /etc/default/keyboard > /dev/null <<'EOF'
@@ -74,14 +92,3 @@ sudo dpkg-reconfigure keyboard-configuration
 #    send "\r"
 #END
 
-# Install ROS
-/bin/echo -e "\e[1;32mInstalling ROS Kinetic.\e[0m"
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-sudo apt-get update -y
-sudo apt-get install ros-kinetic-desktop -y
-sudo rosdep init
-rosdep update
-echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
-source ~/.bashrc
-sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential python-catkin-tools
