3c167e9dc5ae767590ba083b2c3dc2e63696ec66
b51 <b51live@gmail.com>
2019-09-28 11:13:24 +0800

    Add fzf install script

---
+++ ZshSettings/fzfsetup.sh
@@ -0,0 +1,15 @@
+#########################################################################
+#
+#              Author: b51
+#                Mail: b51live@gmail.com
+#            FileName: fzfsetup.sh
+#
+#          Created On: Sat 28 Sep 2019 11:10:30 AM CST
+#
+#########################################################################
+
+#!/bin/bash
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
