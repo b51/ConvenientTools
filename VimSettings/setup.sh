08c9d4073964bd840721a65f936bdb8f68f52f49
b51 <b51live@gmail.com>
2020-03-18 01:39:01 +0800

    Add detail introduction of vim plugins

--- VimSettings/setup.sh
+++ VimSettings/setup.sh
@@ -17,4 +17,4 @@ cd ~/.vim/bundle/
 cat .rosinstall | grep uri | awk '{print $2}' | while read line; do git clone --recursive $line; done;
 cd YouCompleteMe
 python3 install.py --clang-completer
-sudo apt install clang-format
+sudo apt install clang-format exuberant-ctags cscope
